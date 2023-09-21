5c489500962b5a3a6fcd01adce9e5f3eac37e621
zahar.yashin <>
2023-09-21 14:05:46 +0700

    Create project and color

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Yashin Zahar on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoApp/CryptoApp/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Yashin Zahar on 21.09.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
