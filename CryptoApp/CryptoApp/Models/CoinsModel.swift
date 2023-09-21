b292762db7afdca5c219625fa2aedfa55d782063
zahar.yashin <>
2023-09-21 15:08:28 +0700

    Add model coins

---
+++ CryptoApp/CryptoApp/Models/CoinsModel.swift
@@ -0,0 +1,42 @@
+//
+//  CoinsModel.swift
+//  CryptoApp
+//
+//  Created by Yashin Zahar on 21.09.2023.
+//
+
+import Foundation
+
+struct CoinModel: Identifiable, Decodable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H: Double?
+    let priceChange24H, priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+    let currentHoldings: Double?
+    
+    func updateHoldings(amount: Double) -> CoinModel {
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
+    }
+    
+    var currentHoldingsValue: Double {
+        currentPrice * (currentHoldings ?? 0)
+    }
+    
+    var rank: Int {
+        Int(marketCapRank ?? 0)
+    }
+}
+
+struct SparklineIn7D: Decodable {
+    let price: [Double]
+}
